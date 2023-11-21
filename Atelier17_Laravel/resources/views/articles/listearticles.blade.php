751af4d2121bdb24ba3101372314bc7e5f5c1eb6
Yaye Rokhaya <ladyrokhaya@gmail.com>
2023-11-21 19:18:06 +0000

    Affichage effective

--- Atelier17_Laravel/resources/views/articles/listearticles.blade.php
+++ Atelier17_Laravel/resources/views/articles/listearticles.blade.php
@@ -9,14 +9,14 @@
     @endif
     <div class="row mt-2">
         @foreach ($articles as $article)
-            <div class="col-lg-6 mt-4 mb-4 ">
-                <div class="card z-index-4" style="height:70vh;">
+            <div class="col-lg-6 mt-4 mb-4">
+                <div class="card z-index-4" style="height:auto;">
                     <div
                         class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent
-    d-flex justify-content-center align-items-center">
+                                        d-flex justify-content-center align-items-center">
                         <img src="{{ asset('images/' . $article->image) }}" style="width:400px" alt="image de l'article">
                     </div>
-                    <div class="card-body">
+                    <div class="card-body" style="overflow: hidden;">
                         <h6 class="mb-0 ">{{ $article->nom }}</h6>
                         <style>
                             .ellipsis {
@@ -39,15 +39,20 @@
                         </div>
                         <div class="card-body" style="display:flex;justify-content:space-around;flex-wrap:nowrap;">
                             <div>
-                                <a href="/articles/{{ $article->id }}" class="btn btn-primary me-3">voir plus</a>
+                                <a href="/articles/{{ $article->id }}" class="btn btn-success me-3">voir plus</a>
                             </div>
                             <form action="/commentaire" class="comment_class" method="post">
                                 @csrf
+                                <div class="row d-flex justify-content-center align-items-center">
+                                
                                 <input type="hidden" name="article_id" value="{{ $article->id }}">
                                 <input type="hidden" name="user_id" value='{{ Auth::user()->id }}'>
-                                <input type="text" class="input_comment" name="contenu"
-                                    placeholder="Taper ici votre commentaire...">
-                                <button type="submit" class="card__btn">Commenter</button>
+                                <textarea type="text" class="input_comment mb-4" name="contenu"
+                                    placeholder="Taper ici votre commentaire..." rows="2">
+                                    </textarea>
+                                <button type="submit" class="btn btn-warning me-3" style="width: 150px;">Commenter</button>
+                                    
+                            </div>
                             </form>
                         </div>
                     </div>
--- Atelier17_Laravel/resources/views/articles/voirplus.blade.php
+++ Atelier17_Laravel/resources/views/articles/voirplus.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.template')
 @section('content')
-
     @if (session('statut'))
         <div class="row d-flex justify-content-center align-items-center">
             <div class="alert alert-success col-lg-4">
@@ -10,25 +9,23 @@
     @endif
 
     @if (session('status'))
-    <div class="row d-flex justify-content-center align-items-center">
-    <div class="alert alert-success">
-        {{ session('status') }}
-    </div>
-</div>
-
-@endif
-
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        </div>
+    @endif
 
     <div class="container">
         <div class="row mt-5 d-flex justify-content-center align-items-center">
             <div class="col-lg-10 mt-2 mb-4 ">
-                <div class="card z-index-2" style="height:100vh;">
+                <div class="card z-index-2" style="height:auto;">
                     <div
                         class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent
                     d-flex justify-content-center align-items-center">
                         <img src="{{ asset('images/' . $article->image) }}" style="width: 400px" alt="image de l'article">
                     </div>
-                    <div class="card-body">
+                    <div class="card-body mx-6">
                         <h6 class="mb-0 ">{{ $article->nom }}</h6>
                         <p class="text-sm ">{{ $article->description }}</p>
 
@@ -39,32 +36,46 @@
                             <i class="fa-solid fa-location-dot me-2"></i>
                             <p class="text-sm"> {{ $article->localisation }} </p>
                         </div>
-                        <div class="mt-3 d-flex justify-content-center align-items-center">
-                            <a href="{{ '/article/modifier/' . $article->id }}" class="btn btn-primary me-3">modifier</a>
-                            <a href="/articles/deletearticle/{{ $article->id }}" class="btn btn-primary">Supprimer</a>
-            @if (isset($ok) && $commentaire->user_id === Auth::user()->id)
-                <form action="{{ url('/articles/commentaireupdate/' . $commentaire->id) }}" class="comment_class" method="post">
-                    @csrf
-                    <input type="hidden" name="article_id" value="{{ $article->id }}">
-                    <input type="hidden" name="id" value='{{ $commentaire->id }}'>
-                    <input type="text" class="input_comment" name="contenu" value="{{ $commentaire->contenu }}">
-                    <button type="submit" class="card__btn">Modifier</button>
-                </form>
-            @endif
-        </div>
-    </div>
+                        <div class=" d-flex justify-content-center align-items-center">
+                            <a href="{{ '/article/modifier/' . $article->id }}" class="btn btn-success me-3">Modifier Info Bien</a>
+                            <a href="/articles/deletearticle/{{ $article->id }}" class="btn btn-danger">Supprimer le Bien</a>
+                            
+                            @if (isset($ok) && $commentaire->user_id === Auth::user()->id)
+                                <form action="{{ url('/articles/commentaireupdate/' . $commentaire->id) }}"
+                                    class="comment_class" method="post">
+                                    @csrf
+                                    <input type="hidden" name="article_id" value="{{ $article->id }}">
+                                    <input type="hidden" name="id" value='{{ $commentaire->id }}'>
+                                    <input type="text" class="input_comment" name="contenu"
+                                        value="{{ $commentaire->contenu }}">
+                                    <button type="submit" class="card__btn">Modifier</button>
+                                </form>
+                            @endif
 
-    <div class="comments mt-4">
-        @forelse ($article->comments as $comment)
-            <div class="comment" style="display:flex;justify-content:space-around;flex-wrap:nowrap;">
-                <div class="mb-1">Contenu : {{ $comment->contenu }}</div>
-                <div class="text-muted mb-1">Auteur : {{ $comment->user->name }}</div>
-                <div class="text-muted mb-1"> Date de publication : {{ $comment->created_at }}</div> 
-                    <a href="/articles/deletecommentaire/{{ $comment->id }}" class="badge rounded-pill bg-primary">Supprimer</a>
-                    <a href="/articles/commentaire/{{$comment->id}}" class="badge rounded-pill bg-dark">Modifier</a>
+                        </div>
+                    </div>
+                    <hr>
+                    <h3 style="text-align: center">Commentaires</h3>
+                    <div class="comment mx-10 row d-flex justify-content-center align-items-center">
+                        @forelse ($article->comments as $comment)
+                            <div class="comment mx-10 " >
+                                <div class="text-muted mb-2">Auteur : {{ $comment->user->name }}</div>
+                                <div class="mb-2">Contenu : {{ $comment->contenu }}</div>
+                                <div class="text-muted mb-2"> Date: {{ $comment->created_at }}</div>
+                                
+                                <a href="/articles/commentaire/{{ $comment->id }}" class="btn btn-success"style="font-size: 15px;"><i class="fa-solid fa-pen-to-square icon-large"></i></a>
+                                <a href="/articles/deletecommentaire/{{ $comment->id }}"
+                                    class="btn btn-danger" style="font-size: 15px; "><i class="fa-solid fa-delete-left"></i></a>
+
+                            </div>
+                            </div>
+                        @empty
+                            <div class="text-muted ms-12 mb-5 mt-0">
+                                Aucun commentaire pour ce bien immobillier
+                            </div>
+                        @endforelse
+                    </div>
+                </div>
             </div>
-        @empty
-            <p class="text-muted">Pas de commentaire pour cet article</p>
-        @endforelse
-    </div>
-@endsection
+        </div>
+    @endsection
