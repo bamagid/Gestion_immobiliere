7186db8c3dd7203615016b9158c5957487bac614
Yaye Rokhaya <ladyrokhaya@gmail.com>
2023-11-21 11:36:12 +0000

    Arrangement des vues

--- Atelier17_Laravel/routes/web.php
+++ Atelier17_Laravel/routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [ArticleController::class, 'index']);
+Route::get('/articles/listearticles', [ArticleController::class, 'index']);
 
 Route::get('/articles/{id}',[ArticleController::class,'show']);
 
