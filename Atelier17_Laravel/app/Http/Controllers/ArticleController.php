e39c13041c9a58d82b3937cd0bea1a724c859ae4
bamagid <bamagid60@gmail.com>
2023-11-21 11:38:42 +0000

    article controlleur

--- Atelier17_Laravel/app/Http/Controllers/ArticleController.php
+++ Atelier17_Laravel/app/Http/Controllers/ArticleController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Admin;
 use App\Models\Article;
 use App\Models\User;
-use Illuminate\Auth\Access\Gate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
