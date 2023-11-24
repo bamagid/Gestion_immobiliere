f0c9c3e09b5c9618b2943e9498880197a3cd25f0
Yaye Rokhaya <ladyrokhaya@gmail.com>
2023-11-24 17:29:49 +0000

    Just un Merge

--- Atelier17_Laravel/database/seeders/DatabaseSeeder.php
+++ Atelier17_Laravel/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\Role::factory(2)->create();
         //  \App\Models\User::factory(7)->create();
         // \App\Models\Commentaire::factory(10)->create();
-        \App\Models\Article::factory(5)->create();
+        // \App\Models\Article::factory(5)->create();
         // // \App\Models\Chambre::factory(10)->create();
 
         // \App\Models\User::factory()->create([
